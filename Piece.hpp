2abfbe189d1777d913dca1e32a321ee328538900
wojo81 <tylerwojo81@gmail.com>
2020-05-28 13:48:01 -0500

    Created Piece class

--- Piece.hpp
+++ Piece.hpp
@@ -1,3 +1,41 @@
 #pragma once
 
+#include <memory>
+#include <vector>
 #include "Move.hpp"
+
+namespace chess {
+    class Piece {
+    public:
+        Piece(Position const position)
+            : position_{position} {}
+
+        Position GetPosition() const {
+            return position_;
+        }
+
+        bool MoveTo(Position const newPosition) {
+            for (auto const& move : moves_) {
+                if (newPosition == move->GetPosition()) {
+                    hasMoved_ = true;
+                    move->DoAction();
+                    position_ = move->GetPosition();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        virtual void SetMoves() = 0;
+        virtual ~Piece() = default;
+    protected:
+        template<typename MoveType>
+        void AddMove(MoveType move) {
+            moves_.push_back(std::make_unique<MoveType>(std::move(move)))
+        }
+    private:
+        Position position_;
+        std::vector<std::unique_ptr<Move>> moves_;
+        bool hasMoved_ = false;
+    };
+};
