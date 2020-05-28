18af02db6bfcdb248313a8c461e4d3a86cd0e0df
wojo81 <tylerwojo81@gmail.com>
2020-05-28 13:31:29 -0500

    Created Move class

--- Move.hpp
+++ Move.hpp
@@ -1,3 +1,20 @@
 #pragma once
 
 #include "Position.hpp"
+
+namespace chess {
+    class Move {
+    public:
+        Move(Position position)
+            : position_{position} {}
+
+        Position GetPosition() const {
+            return position_;
+        }
+
+        virtual void DoAction() {}
+        virtual ~Move() = default;
+    private:
+        Position position_;
+    };
+};
