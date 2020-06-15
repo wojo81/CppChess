a0b1ae8b00867d4de8c9e96ea177fd4929a2e150
wojo81 <tylerwojo81@gmail.com>
2020-06-14 20:44:48 -0500

    Basic outline for Attack class

---
+++ Attack.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Piece.hpp"
+
+namespace chess {
+    class Attack : public Move {
+    public:
+        Attack(Piece const& attackedPiece) :
+            Move{attackedPiece.GetPosition()}, attackedPiece_{attackedPiece} {}
+
+        auto DoAction() -> void override {
+            
+        }
+    private:
+        Piece const& attackedPiece_;
+    };
+}
